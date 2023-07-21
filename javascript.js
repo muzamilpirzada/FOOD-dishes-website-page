ebb955253cbca2193ab9b42b2aa42a7ab723fab1
Muzamil Pirzada <muzmailali34@gmail.com>
2023-07-21 21:12:37 +0500

    first commit

---
+++ javascript.js
@@ -0,0 +1,25 @@
+const dish =document.getElementById("dish");
+const dish1 =document.getElementById("dish1");
+const dish2 =document.getElementById("dish2");
+const dish3 =document.getElementById("dish3");
+const dish4 =document.getElementById("dish4");
+const dish5 =document.getElementById("dish5");
+
+
+
+dish1.addEventListener('click',function(){
+    dish.src ="Food-dim/dish1.png";
+})
+
+dish2.addEventListener('click',function(){
+    dish.src ="Food-dim/dish2.png";
+})
+dish3.addEventListener('click',function(){
+    dish.src ="Food-dim/dish3.png";
+})
+dish4.addEventListener('click',function(){
+    dish.src ="Food-dim/dish4.png";
+})
+dish5.addEventListener('click',function(){
+    dish.src ="Food-dim/dish5.png";
+})
